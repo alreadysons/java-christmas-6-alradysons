eef5a68e3534aabe6824b95fe8d050ffc387a87d
alreadysons <alreadysons@gmail.com>
2023-11-13 10:19:20 +0900

    입력을 담당하는 InputView, 출력을 담당하는 OutputView 생성, 유효성검사 제작예성

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -1,4 +1,20 @@
 package christmas;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    private static final String GET_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해주세요!)\n";
+    private static final String GET_MENU = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n";
+
+    public static String get_DateList (){
+        System.out.println(GET_DATE);
+        return Console.readLine();
+    }
+
+    public static String get_MenuList(){
+        System.out.println(GET_MENU);
+        return Console.readLine();
+    }
+
 }
--- src/main/java/christmas/OutputView.java
+++ src/main/java/christmas/OutputView.java
@@ -1,4 +1,9 @@
 package christmas;
+import camp.nextstep.edu.missionutils.Console;
 
 public class OutputView {
+    private static final  String CHRISTMAS_WELCOME = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n";
+
+    private static final String CHRISTMAS_LAST = "12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
+
 }
